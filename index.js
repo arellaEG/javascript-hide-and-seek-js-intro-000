cff14a15e0b156d51efd03c574d223f869315f00
Arella Gussow <arellaeg@gmail.com>
2018-06-08 07:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested')
 }
+
+function increaseRankBy(n){
+  var rank=document.querySelectorAll(className='ranked-list'))
+}
