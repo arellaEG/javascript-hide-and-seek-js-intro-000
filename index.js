92298eaec4d045013e724a802549b1c58d140e75
Arella Gussow <arellaeg@gmail.com>
2018-06-08 07:23:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var rank=document.querySelectorAll('.ranked-list li')
   for (let i=0; i<rank.length;i++){
-    parseInt(rank[i].innerHTML)=parseInt(rank[i].innerHtml)+n
+    rank[i].innerHTML=parseInt(rank[i].innerHtml)+n
   }
 }
