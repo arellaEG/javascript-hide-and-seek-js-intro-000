a93437139b888ea877014e2a48971270aec38f1f
Arella Gussow <arellaeg@gmail.com>
2018-06-08 07:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var rank=document.querySelectorAll('.ranked-list')
+  var rank=document.querySelectorAll('.ranked-list li')
   for (let i=0; i<rank.length;i++){
     rank[i].innerHTML+=n
   }
